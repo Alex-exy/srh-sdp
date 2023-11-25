be7af718c251186ea5e8682dd11fad6e14e939e7
pkunray <pkunray@gmail.com>
2023-11-25 11:59:45 +0100

    chore: add Maven support

---
+++ src/test/java/de/srh/library/AppTest.java
@@ -0,0 +1,12 @@
+package de.srh.library;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+  @Test
+  void main() {
+    assumeTrue(true);
+  }
+}
