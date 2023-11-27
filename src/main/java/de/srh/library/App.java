0b65115bcd1746b26bd0c536ef45a41faec8a8d5
pkunray <pkunray@gmail.com>
2023-11-27 15:48:25 +0100

    chore: integrate logback for logging

--- src/main/java/de/srh/library/App.java
+++ src/main/java/de/srh/library/App.java
@@ -1,11 +1,16 @@
 package de.srh.library;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        logger.info( "Hello World!" );
     }
 }
