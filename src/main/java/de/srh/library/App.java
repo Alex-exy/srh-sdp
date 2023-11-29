eced9f2e9dd05e125be1bcdc2aa20e347b15acee
pkunray <pkunray@gmail.com>
2023-11-29 23:32:13 +0100

    chore: swing example

--- src/main/java/de/srh/library/App.java
+++ src/main/java/de/srh/library/App.java
@@ -1,16 +1,16 @@
 package de.srh.library;
 
+import de.srh.library.ui.login.LoginWindow;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Hello world!
- *
- */
+/** Running point of the application */
 public class App {
 
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
-    public static void main( String[] args ) {
-        logger.info( "Hello World!" );
-    }
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+  public static void main(String[] args) {
+    logger.info("Library Management System is starting...");
+    new LoginWindow();
+  }
 }
