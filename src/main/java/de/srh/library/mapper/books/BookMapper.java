deb344965a6048486af024bc8209b59483870310
Kshitij Deshmukh <142789304+stickrundude@users.noreply.github.com>
2023-12-12 11:24:17 +0100

    feat: added mapper and dao classes for book and genre

---
+++ src/main/java/de/srh/library/mapper/books/BookMapper.java
@@ -0,0 +1,12 @@
+package de.srh.library.mapper.books;
+
+import de.srh.library.entity.Book;
+
+
+public interface BookMapper {
+    Book getBookById(Long bookId);
+
+    Book getBookByAuthor(String author);
+
+    int insertBook(Book book);
+}
